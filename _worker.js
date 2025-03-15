4af213a58e78e5bf52b6686e4e49a215340cf154
hujunger <mail@cfip.nyc.mn>
2025-03-15 21:20:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://sub.xf.free.hr/auto
 
 let urls = [];
 let subconverter = "SUBAPI.CMLiussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/moistrr/Clashruleset/refs/heads/main/config/cfguize"; //订阅配置文件
+let subconfig = "https://raw.cfip.nyc.mn/eliangwww/eliang_clash/refs/heads/main/clash_asia.ini?token=12312"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
